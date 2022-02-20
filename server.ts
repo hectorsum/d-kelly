df0d933573f2cd05f045e81213f6a234c048d002
Hector Herrera <hectorsum81@hotmail.com>
2022-02-20 15:39:52 -0500

    update: setting up project backend

--- server.ts
+++ server.ts
@@ -1,16 +1,16 @@
-import express from 'express';
-import cors from 'cors';
-import http from 'http';
-// import {connectToDB} from './config/db'
+import { ConfigServer } from './server/config.server';
+import {connection} from './config/database.config'
+const server = new ConfigServer(); 
 require("dotenv").config();
-const app = express();
 
-const server: http.Server = http.createServer(app);
-const PORT = process.env.PORT || 8000; 
-
-server.listen(PORT, () => {
-  console.log(`Server starting on port:  ${PORT}`);
-})
-app.use(cors());
-
-app.use(express.json());
+function main(){
+  try { 
+    server.start(() => {
+      console.log(`server on port http://localhost:${server.app.get('port')}`)
+      connection()
+    }) 
+  } catch (error) {
+    console.log(error) 
+  }
+} 
+main();
