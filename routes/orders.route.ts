bda0eb77f2af0a1b91a35403c3dbafed7a8b22d2
Hector Herrera <hectorsum81@hotmail.com>
2022-02-20 18:01:16 -0500

    update: orders route updated

--- routes/orders.route.ts
+++ routes/orders.route.ts
@@ -1,8 +1,32 @@
 import { Router } from 'express'
+import { OrderController } from '../controllers/orders.controller';
 
 const app = Router()
-// const controller = new MainController()
+const controller = new OrderController()
 
-// app.get('/', controller.init)
+// @route  GET api/order
+// @des    Get All Customers
+// @access Private
+app.get('/', controller.getAll);
+
+// @route  GET api/order/:id
+// @des    Get Customer by id
+// @access Private
+app.get('/:id', controller.getOne);
+
+// @route  POST api/order
+// @des    Add Customer
+// @access Private
+app.post('/', controller.add);
+
+// @route  PUT api/order/:id
+// @des    Edit Customer by id
+// @access Private
+app.put('/:id', controller.update);
+
+// @route  DELETE api/order/:id
+// @des    Remove customer by id
+// @access Private
+app.delete('/:id', controller.delete);
 
 export default app;
