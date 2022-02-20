16c10ddecc0774cab7b9cf5d5d47cbafdf744841
Hector Herrera <hectorsum81@hotmail.com>
2022-02-20 17:59:25 -0500

    update: customer and order routes added

--- routes/customer.route.ts
+++ routes/customer.route.ts
@@ -1,8 +1,33 @@
 import { Router } from 'express'
+import { CustomerController } from '../controllers/customer.controller';
 
 const app = Router()
-// const controller = new MainController()
+const controller = new CustomerController()
 
-// app.get('/', controller.init)
+// @route  GET api/customer
+// @des    Get All Customers
+// @access Private
+app.get('/', controller.getAll);
+
+// @route  GET api/customer/:id
+// @des    Get Customer by id
+// @access Private
+app.get('/:id', controller.getOne);
+
+// @route  POST api/customer
+// @des    Add Customer
+// @access Private
+app.post('/', controller.add);
+
+// @route  PUT api/customer/:id
+// @des    Edit Customer by id
+// @access Private
+app.put('/:id', controller.update);
+
+// @route  DELETE api/customer/:id
+// @des    Remove customer by id
+// @access Private
+app.delete('/:id', controller.delete);
+
 
 export default app;
