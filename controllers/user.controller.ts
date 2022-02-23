6654a03589af598afd4c81039dbc85e392b6dea3
Hector Herrera <hectorsum81@hotmail.com>
2022-02-22 23:52:27 -0500

    update: adding all methods regarding User

---
+++ controllers/user.controller.ts
@@ -0,0 +1,75 @@
+import { Request, Response } from 'express'
+import { User } from '../models/user.model';
+import bcrypt from 'bcrypt';
+export class UserController {
+  constructor(){}
+  async add(req: Request, res: Response){
+    try {
+      const {email,password} = req.body; 
+      //* Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt); //* Hashing password
+      console.log("hashedPassword: ",hashedPassword);
+      const user = await User.create({
+        email,
+        password: hashedPassword
+      })
+      
+      res.json({
+        ok: true,
+        msg:"Usuario agregado satisfactoriamente!",
+        data: user
+      })
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  }
+  async update(req: Request, res: Response){
+    try {
+      const { id } = req.params;
+      const updatedUser = await User.findByIdAndUpdate(id, req.body);
+      return res.json({
+        ok: true,
+        msg:"Usuario actualizado satisfactoriamente!",
+        updatedUser,
+      });
+    } catch (error) {
+      return res.status(500).send("Server error");
+    }
+  }
+  async delete(req: Request, res: Response){
+    try {
+      const { id } = req.params;
+      await User.findByIdAndDelete(id);
+      return res.json({
+        ok: true,
+        msg:"Usuario eliminado satisfactoriamente!",
+      });
+    } catch (error) {
+      return res.status(500).send("Server error");
+    }
+  }
+  async getOne(req: Request, res: Response){
+    try {
+      const { id } = req.params;
+      const user = await User.findById(id);
+      return res.json({
+        user,
+      });
+    } catch (error) {
+      return res.status(500).send("Server error");
+    }
+  }
+  async getAll(req: Request, res: Response){
+    try {
+      const users = await User.find();
+      return res.json({
+        ok: true,
+        data: users
+      });
+    } catch (error) {
+      return res.status(500).send("Server error");
+    }
+  }
+
+}
