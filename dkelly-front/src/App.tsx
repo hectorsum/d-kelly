e6f2f1b80921d466f12349080bc144062f982bab
Hector Herrera <hectorsum81@hotmail.com>
2022-02-25 00:26:22 -0500

    update: setting up routes

--- dkelly-front/src/App.tsx
+++ dkelly-front/src/App.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-
+import { ChakraProvider } from '@chakra-ui/react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import LoginScreen from './components/Login/LoginScreen';
+import { Provider } from 'react-redux';
+import {store} from './state/store';
+import { SidebarScreen } from './components/Sidebar/SidebarSreen';
+import { CustomerScreen } from './components/Customer/CustomerScreen';
+import { OrderScreen } from './components/Orders/OrderScreen';
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Provider store={store}>
+      <ChakraProvider>
+        <Router>
+          <Routes>
+            <Route path='/' element={<LoginScreen/>}/>
+          </Routes>
+          <SidebarScreen>
+            <Routes>
+              <Route path='/clientes' element={<CustomerScreen/>}/>
+              <Route path='/pedidos' element={<OrderScreen/>}/>
+            </Routes>
+          </SidebarScreen>
+        </Router>
+      </ChakraProvider>
+    </Provider>
   );
 }
 
