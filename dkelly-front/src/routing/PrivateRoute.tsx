adfd8efa1c1be26ea4addce63559558bc826de2b
Hector Herrera <hectorsum81@hotmail.com>
2022-02-25 00:25:17 -0500

    update: private route on hold

---
+++ dkelly-front/src/routing/PrivateRoute.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const PrivateRoute = () => (
+  <></>
+  //const state =
+  // <Route render={
+  //                           props => !isAuthenticated && !loading ? 
+  //                           (<Navigate to="/login"/>) : 
+  //                           (<Component {...props} />)
+  //                         }
+  // />
+)
