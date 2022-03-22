9cbdea80591671aed0b2c6818c27bb75bf918ef0
Hector Herrera <hectorsum81@hotmail.com>
2022-03-21 22:56:15 -0500

    update: setting up cards into dashboard screen

--- dkelly-front/src/components/Dashboard/DashboardScreen.tsx
+++ dkelly-front/src/components/Dashboard/DashboardScreen.tsx
@@ -1,7 +1,38 @@
+import { Flex, SimpleGrid, useColorModeValue } from '@chakra-ui/react'
 import { FC } from 'react'
+import { CartIcon, DocumentIcon, GlobeIcon, WalletIcon } from '../Icons/Icons'
+import MiniStatistics from './components/MIniStatistics'
 
 export const DashboardScreen: FC = (): JSX.Element => {
+  const iconBoxInside = useColorModeValue("white", "white");
   return (
-    <h1>DashboardScreen</h1>
+    <Flex flexDirection='column' pt={{ base: "0px", md: "0px" }}>
+      <SimpleGrid columns={{ sm: 1, md: 2, xl: 4 }} spacing='24px'>
+        <MiniStatistics
+          title={"Today's Moneys"}
+          amount={"$53,000"}
+          percentage={55}
+          icon={<WalletIcon h={"24px"} w={"24px"} color={iconBoxInside} />}
+        />
+        <MiniStatistics
+          title={"Today's Users"}
+          amount={"2,300"}
+          percentage={5}
+          icon={<GlobeIcon h={"24px"} w={"24px"} color={iconBoxInside} />}
+        />
+        <MiniStatistics
+          title={"New Clients"}
+          amount={"+3,020"}
+          percentage={-14}
+          icon={<DocumentIcon h={"24px"} w={"24px"} color={iconBoxInside} />}
+        />
+        <MiniStatistics
+          title={"Total Sales"}
+          amount={"$173,000"}
+          percentage={8}
+          icon={<CartIcon h={"24px"} w={"24px"} color={iconBoxInside} />}
+        />
+      </SimpleGrid>
+    </Flex>
   )
 }
